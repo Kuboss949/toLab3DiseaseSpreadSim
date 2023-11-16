0d3c28ed81f25ccc2b280cc42367d8e440d4928d
Kuboss949 <jakub.lopata@student.pk.edu.pl>
2023-11-16 22:25:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+import vectors.Vector2D;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Specimen.java
@@ -0,0 +1,18 @@
+import vectors.IVector;
+import vectors.Vector2D;
+
+public class Specimen {
+
+    private IState state;
+    private Vector2D velocityVector;
+    private double velocity;
+
+    public Specimen(IState initialState){
+        this.state = initialState;
+    }
+
+    private Vector2D changeVector{
+
+    }
+
+}
