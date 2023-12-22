ee403b88c25a368257cf1c95844f70de821722f0
Kuboss949 <jakub.lopata@student.pk.edu.pl>
2023-12-22 20:34:07 +0100

    IT WORKS2

--- src/SimulationController.java
+++ src/SimulationController.java
@@ -4,6 +4,7 @@ import population.Specimen;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 
 public class SimulationController {
 
@@ -14,7 +15,7 @@ public class SimulationController {
     private final double maxY;
     private int simTime;
     public SimulationController(double m, double n){
-        population = new Population(200, n, m,  true);
+        population = new Population(200, n, m,  false);
         maxX = n;
         maxY = m;
         simTime = 0;
@@ -22,22 +23,26 @@ public class SimulationController {
 
     public void performSimulationStep(){
         population.movePopulation();
-        if(Math.random()>0.5){
+        if(Math.random()>0.3){
             population.addSpecimen();
         }
         simTime++;
     }
 
     public void drawPopulation(Graphics g, int width, int height){
+        try{
         Graphics2D g2 = (Graphics2D) g;
         ArrayList<Specimen> specimenList = population.getSpecimenList();
-        for (var specimen: specimenList) {
-            g2.setColor(specimen.getState().color);
-            int x = (int) (specimen.getX() / maxX * width);
-            int y = (int) (specimen.getY() / maxY * height);
+            for (var specimen: specimenList) {
+                g2.setColor(specimen.getState().color);
+                int x = (int) (specimen.getX() / maxX * width);
+                int y = (int) (specimen.getY() / maxY * height);
 
-            g2.fillOval(x, y, specimenSize, specimenSize);
-        }
+                g2.fillOval(x, y, specimenSize, specimenSize);
+            }
+        }catch(ConcurrentModificationException e){
+
+            }
     }
 
     public Memento saveMemento(){
